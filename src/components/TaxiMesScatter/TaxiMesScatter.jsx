edd011761b9fcf76f3c0fff85624375142baed8c
unknown <ma_dong_liang@163.com>
2022-12-06 18:42:26 +0800

    build TaxiMesScatter & TravelEmissionVary component

--- src/components/TaxiMesScatter/TaxiMesScatter.jsx
+++ src/components/TaxiMesScatter/TaxiMesScatter.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import {React, useEffect} from 'react';
+
+import {drawSpeedScatter, drawDistanceScatter, drawTimeScatter} from './TaxiMesScatter.js';
 
 import './TaxiMesScatter.css';
 
 export default function TaxiMesScatter() {
+  useEffect(()=>{
+    drawSpeedScatter('speedScatterBox');
+    drawDistanceScatter('distanceScatterBox');
+    drawTimeScatter('timeScatterBox');
+  }, []);
   return (
-    <section className='taxiMesScatter'></section>
-  )
+    <section className='taxiMesScatter'>
+      <section className='speedScatter' id='speedScatterBox'></section>
+      <section className='distanceScatter' id='distanceScatterBox'></section>
+      <section className='timeScatter' id='timeScatterBox'></section>
+    </section>
+  );
 }
